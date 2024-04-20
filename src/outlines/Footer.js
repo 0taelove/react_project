0f6cc1fa4b6ca88d81f8b90187fbaa7112eeb0df
0taelove <0taelove@gmail.com>
2024-04-20 21:10:38 +0900

    회원 가입 페이지 구성 중

--- src/outlines/Footer.js
+++ src/outlines/Footer.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-
 const FooterBox = styled.footer`
   min-height: 250px;
-  baackground: #f8f8f8;
+  background: #f8f8f8;
 `;
 
 const Footer = () => {
